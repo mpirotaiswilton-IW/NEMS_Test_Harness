91a2a18cfba4b99b144b74d0c60d1853799f89c3
Max Pirotais-Wilton <max.pirotais-wilton@integration.works>
2024-07-02 16:36:04 +1200

    Created subscriber microservice

---
+++ NEMS_Test_Subscriber/src/main/java/com/iw/nems_test_subscriber/NemsTestSubscriberApplication.java
@@ -0,0 +1,13 @@
+package com.iw.nems_test_subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemsTestSubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemsTestSubscriberApplication.class, args);
+	}
+
+}
